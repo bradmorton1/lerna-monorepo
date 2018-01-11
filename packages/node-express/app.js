5625bddb2fa92c7d50b86513335ea3daa22c985c
bradmorton1 <33905003+bradmorton1@users.noreply.github.com>
2018-01-11 09:56:54 +0000

    added package ungitted

---
+++ packages/node-express/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+let server = app.listen(3000, () => console.log('Example app listening on port 3000'))
+
+var handler = function() {
+    server.close();
+  };
+
+module.exports = server;
+module.exports.handler = handler;
