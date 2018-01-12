d194d9e17744c23a8ffae70c17199de20cf645b1
bradmorton1 <33905003+bradmorton1@users.noreply.github.com>
2018-01-12 09:20:08 +0000

    Added shared resources and fixed copied package.json

--- packages/node-express-copy/app.js
+++ packages/node-express-copy/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const planets = require('../shared-resource/planets.json')
 
-app.get('/', (req, res) => res.send('Hello Moon!'))
+app.get('/', (req, res) => res.send("Hello " + planets.planets[2] + "!"))
 
 let server = app.listen(3000, () => console.log('Example app listening on port 3000'))
 
--- packages/node-express/app.js
+++ packages/node-express/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const planets = require('../shared-resource/planets.json')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send("Hello " + planets.planets[1] + "!"))
 
 let server = app.listen(3000, () => console.log('Example app listening on port 3000'))
 
